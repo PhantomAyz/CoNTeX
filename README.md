166c399b5ab146d224035f6b85a4c67aef582249
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2024-05-07 07:42:35 +0800

    build: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# CoNTeX
+
+A collaborative note-taking web application which uses Markdown and LaTeX syntax
+
+## Required
+
+Use PHP as the primary programming language to be used.
+
+## Overall Planned Features
+
+- Login/SignUp user account system
+- Markdown + LaTeX editor
+- Markdown + LaTeX previewer
+- File-tree for Current Notes in the Vault
+- Notes vault sharing for collaborative editing
+
+### v0.1.0 Features
+
+- Markdown & LaTeX editor
+- Markdown & LaTeX previewer
---
+++ editor.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>CoNTeX: Collaborative Note-taking in Markdown & LaTeX</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="navbar">
+      <a class="navbar-logo" href="#">CoNTeX</a>   
+      <div class="navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="navbar-item">
+            <a class="navbar-link" href="#">Vault</a>
+          </li>
+          <li class="navbar-item">
+            <a class="navbar-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-account">
+        <a class="navbar-account-name" href="#">User</a>   
+        <a class="navbar-account-picture" href="#">
+          <img class="navbar-profile" src="./images/account.png" alt="image">
+        </a>   
+      </div>
+    </div>
+    
+    <div class="main">
+      <div class="half-width" id="main-editor">
+        <!-- Get default mesage on screen -->
+        <?php
+        $file_path = "./README.md";
+
+        if (file_exists($file_path)) {
+            $file_contents = file_get_contents($file_path);
+        }
+        ?>
+        <!-- Display textarea with file contents when no edits are done -->
+        <textarea id="source-text" placeholder="Enter your text here"><?php 
+          echo htmlspecialchars($file_contents); 
+        ?></textarea>
+
+      </div>
+      <div class="half-width" id="main-preview">
+        <div id="render-text">
+          
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
